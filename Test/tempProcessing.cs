bdc8ef23f41b1bf141fc2a97af28ad34f76b16c8
nbollis <nbollis@comcast.net>
2022-10-20 15:37:22 -0500

    Commented out broken test

--- Test/tempProcessing.cs
+++ Test/tempProcessing.cs
@@ -14,21 +14,21 @@ namespace Test
         [Test]
         public static void OutputCharactersForEnum()
         {
-            string[] characterNames = new string[] {"Isaac", "Cain", "Maggy", "Judas", "Samson", "Eve", "Lilith", "Blue Baby",
-                "Lazarus", "The Forgotten", "Eden", "The Lost", "The Keeper", "Azazel", "Apollyon", "Guppy", "Bum-Bo", "Whore of Babylon",
-                "Dark Judas", "Tapeworm", "Albert", "Bethany", "Jacob & Esau", "The Broken", "The Hoarder", "The Duantless", "The Deceiver", "The Savage",
-                "The Curdled", "The Harlot", "The Soiled", "The Enigma", "The Fettered", "The Capricious", "The Baleful", "The Miser", "The Benighted",
-                "The Empty", "The Zealot", "The Deserter", "Ash", "Guy Spelunky", "The Silent", "Captain Viridian", "The Knight", "Pink Knight",
-                "Boyfriend", "Psycho Goreman", "Blind Johnny", "Salad Fingers", "Blue Archer", "Quote", "Crewmate", "Bum-Bo The Weird", "Steven",
-                "Johnny", "Baba", "Edmund", "Flash Isaac"};
+            //string[] characterNames = new string[] {"Isaac", "Cain", "Maggy", "Judas", "Samson", "Eve", "Lilith", "Blue Baby",
+            //    "Lazarus", "The Forgotten", "Eden", "The Lost", "The Keeper", "Azazel", "Apollyon", "Guppy", "Bum-Bo", "Whore of Babylon",
+            //    "Dark Judas", "Tapeworm", "Albert", "Bethany", "Jacob & Esau", "The Broken", "The Hoarder", "The Duantless", "The Deceiver", "The Savage",
+            //    "The Curdled", "The Harlot", "The Soiled", "The Enigma", "The Fettered", "The Capricious", "The Baleful", "The Miser", "The Benighted",
+            //    "The Empty", "The Zealot", "The Deserter", "Ash", "Guy Spelunky", "The Silent", "Captain Viridian", "The Knight", "Pink Knight",
+            //    "Boyfriend", "Psycho Goreman", "Blind Johnny", "Salad Fingers", "Blue Archer", "Quote", "Crewmate", "Bum-Bo The Weird", "Steven",
+            //    "Johnny", "Baba", "Edmund", "Flash Isaac"};
 
-            using (StreamWriter writer = new(File.Create(@"C:\Users\nboll\characters.txt")))
-            {
-                foreach (var character in characterNames)
-                {
-                    writer.WriteLine(character.Replace(" ", "") + ",");
-                }
-            }
+            //using (StreamWriter writer = new(File.Create(@"C:\Users\nboll\characters.txt")))
+            //{
+            //    foreach (var character in characterNames)
+            //    {
+            //        writer.WriteLine(character.Replace(" ", "") + ",");
+            //    }
+            //}
         }
 
         [Test]
